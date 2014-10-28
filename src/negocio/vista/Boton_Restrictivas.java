b88bb5fcf737c2144320935af81a3d76cef940ed
Ruben <Ruben@Ruben-PC>
2014-10-28 10:35:16 +0100

    Añado clase Boton_Restrictivas

---
+++ src/negocio/vista/Boton_Restrictivas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio.vista;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Boton_Restrictivas extends JButton{
+     public Boton_Restrictivas(){
+        setText("Restrictivas");
+        setBackground(Color.GRAY);
+        setForeground(Color.BLACK);
+    
+    }
+    
+}
