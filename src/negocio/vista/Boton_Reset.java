d932f51ee2bf8ef58d9a152d33405441db51840d
Ruben <Ruben@Ruben-PC>
2014-10-28 10:35:00 +0100

    Añado clase Boton_Reset

---
+++ src/negocio/vista/Boton_Reset.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio.vista;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Boton_Reset extends JButton{
+     public Boton_Reset(){
+        setText("Reset");
+        setBackground(Color.WHITE);
+        setForeground(Color.RED);
+    
+    }
+    
+}
