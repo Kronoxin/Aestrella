ff1d03a1c489bd333cfcdef30ea56baa464ce146
Ruben <Ruben@Ruben-PC>
2014-10-28 10:34:44 +0100

    Añado clase Boton_Prohibidas

---
+++ src/negocio/vista/Boton_Prohibidas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio.vista;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Ruben
+ */
+
+//otro coment
+public class Boton_Prohibidas extends JButton{
+    
+     public Boton_Prohibidas(){
+        setText("Prohibidas");
+        setBackground(Color.BLACK);
+        setForeground(Color.WHITE);
+    
+    }
+    
+}
