7be958c878e771d73892502d69ac154055e13ae9
Ruben <Ruben@Ruben-PC>
2014-10-28 10:34:13 +0100

    Añado clase Boton_Meta

---
+++ src/negocio/vista/Boton_Meta.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio.vista;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Ruben
+ */
+//coment
+public class Boton_Meta extends JButton{
+    
+     public Boton_Meta(){
+        setText("Meta");
+        setBackground(Color.GREEN);
+        setForeground(Color.BLACK);
+    
+    }
+    
+}
